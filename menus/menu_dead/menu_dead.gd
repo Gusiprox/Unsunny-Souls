6116deb1d4fc97b8d59bb79cdff9a32bd406feb6
SaraPF3 <sarapefe5@gmail.com>
2026-02-22 17:14:21 +0100

    Return funciona

--- menus/menu_dead/menu_dead.gd
+++ menus/menu_dead/menu_dead.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_btn_reload_pressed():
-	get_tree().change_scene_to_file("res://enviroment/environment.tscn")	
+	get_tree().change_scene_to_file("res://environment/levels/nivel_1.tscn")	
 
 
 func _on_btn_exit_pressed() -> void:
