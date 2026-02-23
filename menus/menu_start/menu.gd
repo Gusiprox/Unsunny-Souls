e4b8fd4707005ebd8b24de78800784d1b3812d1e
SaraPF3 <sarapefe5@gmail.com>
2026-02-23 23:47:54 +0100

    Se ha cambiado la fuente de los botones

--- menus/menu_start/menu.gd
+++ menus/menu_start/menu.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var conNiveles = $panelNiveles/scrollNiveles/centerNiveles/conNiveles
 @onready var btnRandom = $panelNiveles/btnRandom
 @onready var btnReturn = $panelNiveles/btnReturn
+@export var btnFont: FontFile = preload("res://menus/menu_start/font/antiquity-print.ttf")
+
 
 var niveles = [
 	{"ruta": "res://environment/levels/nivel_1.tscn", "nombre": "Bosque"},
@@ -27,6 +29,11 @@ func crearBotonesNiveles():
 	for i in range(niveles.size()):
 		var btn = Button.new()
 		btn.text = niveles[i]["nombre"]
+		
+		if btnFont:
+			btn.add_theme_font_override("font", btnFont)
+			btn.add_theme_font_size_override("font_size", 14)
+			
 		btn.pressed.connect(Callable(self, "_onNivelPressed").bind(niveles[i]["ruta"]))
 		conNiveles.add_child(btn)
 
