b2fcd12dc7420509f25c6ebd7a5179f0be43a095
SaraPF3 <sarapefe5@gmail.com>
2026-02-23 23:16:22 +0100

    Añadido botón que lleva a menú principal

--- menus/menu_dead/menu_dead.gd
+++ menus/menu_dead/menu_dead.gd
@@ -6,5 +6,9 @@ func _on_btn_reload_pressed():
 	get_tree().change_scene_to_file(ruta)
 
 
+func _on_btn_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://menus/menu_start/menu.tscn")
+
+
 func _on_btn_exit_pressed() -> void:
 	get_tree().quit()
