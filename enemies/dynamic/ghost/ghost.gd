45d7d0eb6d8752e02d6815e09c236184d0a2d9fe
Aitorre <aitor.rebato@gmail.com>
2026-02-24 02:37:49 +0100

    Comentarios
    Añadido comentarios en el código de los enemigos para facilitar su comprensión

--- enemies/dynamic/ghost/ghost.gd
+++ enemies/dynamic/ghost/ghost.gd
@@ -25,6 +25,7 @@ func _ready() -> void:
 	add_to_group(groupEnemies)
 	enemyAnimations.play(idleAnimation)
 
+# De normal la velocidad la tendremos en 0 al empezar para que solo pueda subir y bajar cuando el jugador esté lo suficientemente cerca
 func _physics_process(_delta: float) -> void:
 	velocity.y = Verticalspeed
 		
@@ -48,10 +49,12 @@ func _physics_process(_delta: float) -> void:
 		
 	move_and_slide()
 
+# Importante que la función morir no sea quien devuelva los puntos para lograr que el enemigo se autoddestruya al atacar al jugador
 func _dealDamage() -> int:
 	die()
 	return points
 
+# Al autodestruirse al atacar llamará a la función de morir
 func _on_ghost_ar_atk_body_entered(body: Node2D) -> void:
 	if body.is_in_group(groupPlayer):
 		body._dealDamage()
@@ -67,6 +70,7 @@ func _on_ghost_ar_search_right_body_entered(body: Node2D) -> void:
 		horizontalWay = 1
 		enemyAnimations.flip_h = false
 
+# Si el jugador se acerca por arriba o abajo, haremos que el valor de velocidad vertical sea igual que el de la horizontal para que sean iguales y con el uso de los sentidos verticales podremos indicar si subir o bajar
 func _on_ghost_ar_search_up_body_entered(body: Node2D) -> void:
 	if body.is_in_group(groupPlayer):
 		verticalWay = -1
@@ -76,7 +80,8 @@ func _on_ghost_ar_search_down_body_entered(body: Node2D) -> void:
 	if body.is_in_group(groupPlayer):
 		verticalWay = 1
 		Verticalspeed = Horizontalspeed
-
+		
+# Cuando se salga el jugador de las colisiones de busqueda verticales se dejará de moverse arriba y abajo para evitar salirse del nivel o de quedarse atascado en el suelo
 func _on_ghost_ar_search_up_body_exited(body: Node2D) -> void:
 	if body.is_in_group(groupPlayer):
 		Verticalspeed = 0
@@ -104,4 +109,5 @@ func die() -> void:
 	deathAudio.play()
 	enemyAnimations.play(deathAnimation)
 	await enemyAnimations.animation_finished
+	
 	queue_free()
--- enemies/dynamic/undead/undead.gd
+++ enemies/dynamic/undead/undead.gd
@@ -51,21 +51,27 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+# Importante que toda la lógica de morir esté en otra función ya que si no es así los puntos se obtendrían al terminar la animación de muerte
 func _dealDamage() -> int:
 	die()
 	return points
 
+# Si el jugador toca al enemigo, el enemigo dejará de moverse y de intentar de volver a atacar al jugador hasta que termine la animación de ataque
 func _on_undead_ar_atk_body_entered(body: Node2D) -> void:
 	if body.is_in_group(groupPlayer):
 		body._dealDamage()
 		enemyAnimations.play(attackAnimation)
+		
 		enemyAttackCollisions.set_deferred("disabled", true)
 		set_physics_process(false)
+		
 		await enemyAnimations.animation_finished
+		
 		enemyAttackCollisions.set_deferred("disabled", false)
 		set_physics_process(true)
 		enemyAnimations.play(idleAnimation)
 
+# Cuando el jugador se acerque lo suficiente el enemigo se girará a por él
 func _on_undead_ar_search_left_body_entered(body: Node2D) -> void:
 	if body.is_in_group(groupPlayer):
 		way = -1
@@ -76,14 +82,19 @@ func _on_undead_ar_search_right_body_entered(body: Node2D) -> void:
 		way = 1
 		enemyAnimations.flip_h = false
 
+# Al girarse se llamará a esta función para que busque al jugador de nuevo y vuelva a girarse a por él
 func resetSearchCollisions() -> void:
 	TimerReactivateSearch.start()
+	
 	enemyRightSearchCollisions.set_deferred("disabled", true)
 	enemyLeftSearchCollisions.set_deferred("disabled", true)
+	
 	await TimerReactivateSearch.timeout
+	
 	enemyRightSearchCollisions.set_deferred("disabled", false)
 	enemyLeftSearchCollisions.set_deferred("disabled", false)
 
+# Al morir se desactivan todas las colisiones para que el jugador no se choque con el enemigo miestras muere y no pueda ser atacado
 func die() -> void:
 	enemyCollisions.set_deferred("disabled", true)
 	attackArea.set_deferred("monitoring", false)
@@ -91,6 +102,6 @@ func die() -> void:
 	
 	deathAudio.play()
 	enemyAnimations.play(deathAnimation)
-	
 	await enemyAnimations.animation_finished
+	
 	queue_free()
